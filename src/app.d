fad2416ecb5f5fc9415d6a25793a7a8c62f194ce
Mrokkk <mrokk@protonmail.com>
2017-03-16 20:23:10 +0100

    Move routing to the separate D-file, add searching

--- src/app.d
+++ src/app.d
@@ -1,35 +1,8 @@
-import std.file;
-import std.stdio: writeln;
-import std.path: baseName, absolutePath;
-
 import vibe.vibe;
 import vibe.appmain;
-import vibe.http.router;
 import vibe.http.server;
 import vibe.http.fileserver;
-
-void handleFile(string filename, HTTPServerResponse res) {
-    if (filename.endsWith(".html")) {
-        res.writeBody(filename.readText(), "text/html");
-    }
-    else {
-        res.writeBody(filename.readText());
-    }
-}
-
-void handleRequest(scope HTTPServerRequest req, scope HTTPServerResponse res) {
-    string title = "Directory listing " ~ req.path;
-    auto filename = req.path[1..$];
-    writeln(req.path);
-    if (filename.length) {
-        if (!filename.isDir()) {
-            handleFile(filename, res);
-            return;
-        }
-    }
-    auto files = dirEntries(filename, "*", SpanMode.shallow, false);
-    res.render!("index.dt", title, files, baseName);
-}
+import router: createRouter;
 
 void configureLogger() {
     auto logger = cast(shared)new HTMLLogger("log.html");
@@ -41,15 +14,6 @@ HTTPServerSettings createHTTPSettings() {
     settings.port = 8080;
     settings.bindAddresses = ["0.0.0.0"];
     return settings;
-}
-
-URLRouter createRouter() {
-    auto router = new URLRouter;
-    auto fsettings = new HTTPFileServerSettings;
-    fsettings.serverPathPrefix = "/static";
-    router.get("/static/*", serveStaticFiles("./public/", fsettings));
-    router.get("/*", &handleRequest);
-    return router;
 }
 
 void main() {
---
+++ src/router.d
@@ -0,0 +1,60 @@
+module router;
+import std.file;
+import std.stdio: writeln;
+import std.path;
+import std.regex;
+import std.algorithm;
+
+import vibe.vibe;
+import vibe.http.router;
+
+void handleFile(string filename, HTTPServerResponse res) {
+    if (filename.endsWith(".html")) {
+        res.writeBody(filename.readText(), "text/html");
+    }
+    else {
+        res.writeBody(filename.readText());
+    }
+}
+
+void handleDir(string dirName, HTTPServerResponse res) {
+    auto files = dirEntries(dirName, SpanMode.shallow, false);
+    auto baseDir = "/" ~ dirName;
+    auto title = "Directory listing" ~ baseDir;
+    res.render!("index.dt", title, files, baseDir);
+}
+
+void handleSearch(string dirName, string query, HTTPServerResponse res) {
+    auto regex = std.regex.regex!string(".*" ~ query ~ ".*", "i");
+    auto files = dirEntries(dirName, SpanMode.depth)
+        .filter!(a => a.name.match(regex));
+    auto baseDir = "/" ~ dirName;
+    auto title = "Search result for " ~ query ~ " in " ~ baseDir;
+    res.render!("index.dt", title, files, baseDir);
+}
+
+void handleRequest(scope HTTPServerRequest req, scope HTTPServerResponse res) {
+    auto filename = req.path[1..$];
+    auto query = req.query;
+    if (query.length) {
+        handleSearch(filename, query["search"], res);
+        return;
+    }
+    writeln(req.path);
+    if (filename.length) {
+        if (!filename.isDir()) {
+            handleFile(filename, res);
+            return;
+        }
+    }
+    handleDir(filename, res);
+}
+
+URLRouter createRouter() {
+    auto router = new URLRouter;
+    auto fsettings = new HTTPFileServerSettings;
+    fsettings.serverPathPrefix = "/static";
+    router.get("/static/*", serveStaticFiles("./public/", fsettings));
+    router.get("/*", &handleRequest);
+    return router;
+}
