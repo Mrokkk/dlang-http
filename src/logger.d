6d07cb72a86aea7179fc8056bcc978cb111156c6
Mrokkk <mrokk@protonmail.com>
2017-04-02 18:30:34 +0200

    WIP: logger

--- src/logger.d
+++ src/logger.d
@@ -9,7 +9,22 @@ static class Logger {
 
     static void log(HTTPServerRequest req, HTTPServerResponse res) {
         auto dateTime = Clock.currTime();
-        writeln(dateTime, " ", req.peer, " ", req.method, " ", req.requestURL, " ", req.headers["User-Agent"]);
+        string color;
+        auto reset = "\033[0m";
+        switch (req.method) {
+            case HTTPMethod.GET:
+                color = "\033[1;32m";
+                break;
+            case HTTPMethod.POST:
+                color = "\033[1;35m";
+                break;
+            case HTTPMethod.HEAD:
+                color = "\033[1;36m";
+                break;
+            default:
+                break;
+        }
+        writeln(dateTime.toSimpleString(), " ", req.peer, " ", color, req.method, " ", req.requestURL, " ", req.headers["User-Agent"], reset);
     }
 
 };
