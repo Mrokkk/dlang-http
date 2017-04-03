d6d557f8d58aae62e61d10a3ffbc35bea9b5e8f6
Mrokkk <mrokk@protonmail.com>
2017-04-03 15:30:12 +0200

    Refactoring in api.d

--- src/api.d
+++ src/api.d
@@ -1,15 +1,15 @@
 module api;
 
 import std.uri: decode;
+import std.path: baseName;
 import std.datetime: Clock;
 import std.regex: regex, match;
 import std.exception: collectException;
-import std.algorithm: filter, startsWith;
-import std.path: baseName, asRelativePath;
+import std.algorithm: filter, startsWith, canFind;
 import std.file: isFile, exists, dirEntries, SpanMode, getcwd;
 
-import vibe.http.server: HTTPServerRequest, HTTPServerResponse;
 import vibe.data.json: serializeToJson, deserializeJson;
+import vibe.http.server: HTTPServerRequest, HTTPServerResponse;
 
 struct Response {
 
@@ -48,38 +48,27 @@ void handleSearch(string dirName, string query, HTTPServerResponse res) {
 void handleApi(scope HTTPServerRequest req, scope HTTPServerResponse res) {
     Request request = deserializeJson!Request(req.queryString.decode);
     Response response;
-    if (request.path == "") {
+    auto path = "." ~ request.path;
+    if (request.path == "" || path.canFind("//") || !path.exists()) {
         res.statusCode = 404;
         return;
     }
-    auto path = request.path[1..$];
-    if (path != "") {
-        if (asRelativePath(path, getcwd()).startsWith("..")) {
-            res.statusCode = 404;
+    if (path.isFile()) {
+        response.file = true;
+        res.writeJsonBody = response.serializeToJson();
+    }
+    else {
+        if (request.search != "") {
+            handleSearch(path, request.search, res);
             return;
         }
-        if (!path.exists()) {
-            res.statusCode = 404;
-            return;
+        response.entries ~= Response.Entry("..", 0, "", true);
+        auto files = dirEntries(path, SpanMode.shallow, false);
+        foreach (file; files) {
+            response.entries ~= Response.Entry(file.name, file.size, file.timeLastModified.toISOExtString(), file.isDir);
         }
-        if (path.isFile()) {
-            response.file = true;
-            res.writeJsonBody = response.serializeToJson();
-            return;
-        }
-        else {
-            response.entries ~= Response.Entry("..", 0, "", true);
-        }
+        response.file = false;
+        res.writeJsonBody = response.serializeToJson();
     }
-    if (request.search != "") {
-        handleSearch(path, request.search, res);
-        return;
-    }
-    auto files = dirEntries(path, SpanMode.shallow, false);
-    foreach (file; files) {
-        response.entries ~= Response.Entry(file.name, file.size, file.timeLastModified.toISOExtString(), file.isDir);
-    }
-    response.file = false;
-    res.writeJsonBody = response.serializeToJson();
     return;
 }
